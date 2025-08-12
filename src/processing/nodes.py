16e2c3c0bc58211629bc56df55049994db2ba445
IngvarsV <148284591+IngvarsV@users.noreply.github.com>
2025-08-12 14:18:29 +0100

    Minor adjustment to calculate the standard error.

--- src/processing/nodes.py
+++ src/processing/nodes.py
@@ -8,7 +8,7 @@ import os.path
 FILES = 1024
 
 # Plotting parameters
-RANGE = 8.0
+RANGE = 4.0
 BINS = 1024
 
 print("Reading data files...")
@@ -54,6 +54,9 @@ binned_signs = sp.stats.binned_statistic_2d(
 binned_deviations = sp.stats.binned_statistic_2d(
     data[:, 0], data[:, 1], data[:, 2], bins=[coordinate_bins, coordinate_bins], statistic='std')
 
+binned_counts = sp.stats.binned_statistic_2d(
+    data[:, 0], data[:, 1], None, bins=[coordinate_bins, coordinate_bins], statistic='count')
+
 # Set empty bins to zero and calculate scale bounds
 pixel_map = np.nan_to_num(binned_signs.statistic.T)
 bound = np.max(np.abs(pixel_map))
@@ -71,12 +74,13 @@ mp.savefig('src/processing/output/signs.png', dpi=BINS *
 mp.clf()
 
 # Plot the relative standard deviation
-pixel_map = binned_deviations.statistic.T / binned_signs.statistic.T
-pixel_map = np.nan_to_num(pixel_map)
-pixel_map = np.abs(pixel_map)
+error_map = binned_deviations.statistic.T / \
+    binned_signs.statistic.T / np.sqrt(binned_counts.statistic.T)
+error_map = np.nan_to_num(error_map)
+error_map = np.abs(error_map)
 
 image = mp.imshow(
-    pixel_map, extent=[-RANGE, RANGE, -RANGE, RANGE], vmin=0.0, vmax=0.2)
+    error_map, extent=[-RANGE, RANGE, -RANGE, RANGE], vmin=0.0, vmax=0.2)
 mp.colorbar(image)
 
-mp.savefig('src/processing/output/deviations.png', dpi=300)
+mp.savefig('src/processing/output/errors.png', dpi=300)
