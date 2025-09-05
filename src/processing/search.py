53667369997a2e53aecf9615b4608a5b7f6f32e1
IngvarsV <148284591+IngvarsV@users.noreply.github.com>
2025-09-05 14:31:02 +0100

    Final minor tweaks for smooth functioning.

--- src/processing/search.py
+++ src/processing/search.py
@@ -7,7 +7,7 @@ import subprocess
 import os
 
 # Amount of simulations for a single point
-N = 4
+N = 16
 
 
 def var(parameters):
@@ -38,7 +38,10 @@ def var(parameters):
             print(
                 f"Simulation {str(ids[process_index])} produced {data_points.shape[0]} data points!")
 
-            if process_index == 0:
+            if data_points.shape[0] != 5000:
+                continue
+
+            if 'cumulative_data' not in locals():
                 cumulative_data = data_points
             else:
                 cumulative_data = np.vstack((cumulative_data, data_points))
@@ -59,16 +62,16 @@ def var(parameters):
     energy_variance = stats.mstats.trimmed_var(
         cumulative_data[thermalisation_cutoff:, 0], 0.02)
 
-    # Calculate variance of the energy by removing 2% of outliers
-    return energy_average / adjustment_average * np.sqrt(energy_variance ** 2 / energy_average ** 2 + adjustment_variance ** 2 / adjustment_average**2)
+    # Calculate variance squared of the energy by removing 2% of outliers
+    return energy_average ** 2 / adjustment_average ** 2 * (energy_variance ** 2 / energy_average ** 2 + adjustment_variance ** 2 / adjustment_average**2)
 
 
 if __name__ == "__main__":
-    result = bb.minimize(f=var, domain=[[-10.0, -20.0], [
-                         0.025, 0.075]], budget=96, batch=24)
+    result = bb.minimize(f=var, domain=[[0.0, -30.0], [
+                         0.01, 0.1]], budget=24, batch=6)
 
     print(
-        f"Found best energy variance of value {-result['best_f']} at {result['best_x']}!")
+        f"Found best energy variance of value {result['best_f']} at {result['best_x']}!")
 
     print(
         f"Data was gathered at backflow parameters: \n{result['all_xs']}\nThe corresponding energy variance values were:\n{result['all_fs']}")
