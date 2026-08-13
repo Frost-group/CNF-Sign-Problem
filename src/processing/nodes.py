1f8e451afd5a0e9da66d062990f37779a1b2cc7e
IngvarsV <148284591+IngvarsV@users.noreply.github.com>
2026-08-13 20:32:29 +0100

    Bug fix.

--- src/processing/nodes.py
+++ src/processing/nodes.py
@@ -103,9 +103,9 @@ for m in range(MOMENTA):
     average_y = np.mean(1 / data[:, 1] ** (m + 1))
 
     momenta[m, 0] = np.mean(momenta_x) / average_x
-    momenta[m, 1] = np.std(momenta_x) / average_x / data.shape[0]
+    momenta[m, 1] = np.std(momenta_x) / average_x / np.sqrt(data.shape[0])
 
     momenta[m, 2] = np.mean(momenta_y) / average_y
-    momenta[m, 3] = np.std(momenta_y) / average_y / data.shape[0]
+    momenta[m, 3] = np.std(momenta_y) / average_y / np.sqrt(data.shape[0])
 
 np.savetxt("src/processing/output/momenta.csv", momenta, delimiter=", ")
